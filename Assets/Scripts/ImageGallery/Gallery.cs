ffd53d3a561b272f42b9372b47628df5fbd913e8
Shaun Evans <shaunevans2000@gmail.com>
2018-05-03 15:42:17 -0700

    Sharing!

--- Assets/Scripts/ImageGallery/Gallery.cs
+++ Assets/Scripts/ImageGallery/Gallery.cs
@@ -36,13 +36,33 @@ public class Gallery : MonoBehaviour
     public void OnShareButtonClick()
     {
         Debug.Log("Share!");
-        var page = scrollSnapRect.GetNearestPage();
-        Debug.Log("Page:"+page);
+        var pageIndex = scrollSnapRect.GetNearestPage();
+        Debug.Log("Page:"+pageIndex);
+
+        var file = imageLoader.files[pageIndex];
+        Debug.Log("File "+file);
+
+        ShareUtility.ShareImage(file);
     }
 
     public void OnEditButtonClick()
     {
         Debug.Log("Edit!");
+
+        AndroidJavaClass intentClass = new AndroidJavaClass("android.content.Intent");
+        AndroidJavaObject intentObject = new AndroidJavaObject("android.content.Intent");
+
+        // Construct the intent.
+        AndroidJavaObject intent = new AndroidJavaObject("android.content.Intent");
+        intent.Call<AndroidJavaObject>("setAction", intentClass.GetStatic<string>("ACTION_SEND"));
+        intent.Call<AndroidJavaObject>("putExtra", intentClass.GetStatic<string>("EXTRA_TEXT"), "Here's the text I want to share.");
+        intent.Call<AndroidJavaObject>("setType", "text/plain");
+
+        // Display the chooser.
+        AndroidJavaClass unityPlayerClass = new AndroidJavaClass("com.unity3d.player.UnityPlayer");
+        AndroidJavaObject currentActivity = unityPlayerClass.GetStatic<AndroidJavaObject>("currentActivity");
+        AndroidJavaObject chooser = intentClass.CallStatic<AndroidJavaObject>("createChooser", intent, "Share");
+        currentActivity.Call("startActivity", chooser);
     }
 
     public void OnDeleteButtonClick()
---
+++ Assets/Scripts/Utility/ShareUtility.cs
@@ -0,0 +1,107 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+
+public class ShareUtility
+{
+    
+    public static void ShareImage( FileInfo image )
+    {
+        var imageUrl = image.FullName;
+        /*
+        //Works:
+        AndroidJavaClass intentClass = new AndroidJavaClass("android.content.Intent");
+        AndroidJavaObject intentObject = new AndroidJavaObject("android.content.Intent");
+        intentObject.Call<AndroidJavaObject>("setAction", intentClass.GetStatic<string>("ACTION_SEND"));
+        AndroidJavaClass uriClass = new AndroidJavaClass("android.net.Uri");
+        AndroidJavaObject uriObject = uriClass.CallStatic<AndroidJavaObject>("parse", "file://" + imageUrl);
+
+        intentObject.Call<AndroidJavaObject>("putExtra", intentClass.GetStatic<string>("EXTRA_STREAM"), uriObject);
+        intentObject.Call<AndroidJavaObject>("putExtra", intentClass.GetStatic<string>("EXTRA_TEXT"), "Extra text Mozaic " + "link");
+        intentObject.Call<AndroidJavaObject>("putExtra", intentClass.GetStatic<string>("EXTRA_SUBJECT"), "Subject Mozaic");
+        intentObject.Call<AndroidJavaObject>("setType", "image/png");
+        AndroidJavaClass unity = new AndroidJavaClass("com.unity3d.player.UnityPlayer");
+        AndroidJavaObject currentActivity = unity.GetStatic<AndroidJavaObject>("currentActivity");
+        currentActivity.Call("startActivity", intentObject);
+        */
+
+
+        AndroidJavaClass intentClass = new AndroidJavaClass("android.content.Intent");
+        AndroidJavaObject intentObject = new AndroidJavaObject("android.content.Intent");
+        intentObject.Call<AndroidJavaObject>("setAction", intentClass.GetStatic<string>("ACTION_SEND"));
+        AndroidJavaClass uriClass = new AndroidJavaClass("android.net.Uri");
+        AndroidJavaObject uriObject = uriClass.CallStatic<AndroidJavaObject>("parse", "file://" + imageUrl);
+
+        intentObject.Call<AndroidJavaObject>("putExtra", intentClass.GetStatic<string>("EXTRA_STREAM"), uriObject);
+        intentObject.Call<AndroidJavaObject>("putExtra", intentClass.GetStatic<string>("EXTRA_TEXT"), "Extra text Mozaic " + "link");
+        intentObject.Call<AndroidJavaObject>("putExtra", intentClass.GetStatic<string>("EXTRA_SUBJECT"), "Subject Mozaic");
+        intentObject.Call<AndroidJavaObject>("setType", "image/png");
+
+        // Display the chooser.
+        AndroidJavaClass unityPlayerClass = new AndroidJavaClass("com.unity3d.player.UnityPlayer");
+        AndroidJavaObject currentActivity = unityPlayerClass.GetStatic<AndroidJavaObject>("currentActivity");
+        AndroidJavaObject chooser = intentClass.CallStatic<AndroidJavaObject>("createChooser", intentObject, "Share via");
+        currentActivity.Call("startActivity", chooser);
+        /*
+        AndroidJavaClass intentClass = new AndroidJavaClass("android.content.Intent");
+        AndroidJavaObject intentObject = new AndroidJavaObject("android.content.Intent");
+
+        // Construct the intent.
+        AndroidJavaObject intent = new AndroidJavaObject("android.content.Intent");
+        intent.Call<AndroidJavaObject>("setAction", intentClass.GetStatic<string>("ACTION_SEND"));
+        intent.Call<AndroidJavaObject>("putExtra", intentClass.GetStatic<string>("EXTRA_TEXT"), text); //Here is the text to share
+        intent.Call<AndroidJavaObject>("setType", "image/png");
+
+        // Display the chooser.
+        AndroidJavaClass unityPlayerClass = new AndroidJavaClass("com.unity3d.player.UnityPlayer");
+        AndroidJavaObject currentActivity = unityPlayerClass.GetStatic<AndroidJavaObject>("currentActivity");
+        AndroidJavaObject chooser = intentClass.CallStatic<AndroidJavaObject>("createChooser", intent, "Share");
+        currentActivity.Call("startActivity", chooser);
+
+
+
+        AndroidJavaClass intentClass = new AndroidJavaClass("android.content.Intent");
+        AndroidJavaObject intentObject = new AndroidJavaObject("android.content.Intent");
+
+        intentObject.Call("setAction", intentClass.GetStatic<string>("ACTION_SEND"));
+        intentObject.Call("setType", "image/*");
+        intentObject.Call("putExtra", intentClass.GetStatic<string>("EXTRA_SUBJECT"), "Media Sharing");
+        intentObject.Call("putExtra", intentClass.GetStatic<string>("EXTRA_TITLE"), "Media Sharing");
+        intentObject.Call("putExtra", intentClass.GetStatic<string>("EXTRA_TEXT"), "Media Sharing android demo mozaic");
+
+        AndroidJavaClass uriClass = new AndroidJavaClass("android.net.Uri");
+        AndroidJavaClass fileClass = new AndroidJavaClass("java.io.File");
+        
+        AndroidJavaObject fileObject = new AndroidJavaObject("java.io.File", imageUrl);// Set Image Path Here
+        AndroidJavaObject uriObject = uriClass.CallStatic<AndroidJavaObject>("fromFile", fileObject);
+
+        bool fileExists = fileObject.Call<bool>("exists");
+        if (fileExists)
+            intentObject.Call("putExtra", intentClass.GetStatic<string>("EXTRA_STREAM"), uriObject);
+
+        AndroidJavaClass unity = new AndroidJavaClass("com.unity3d.player.UnityPlayer");
+        AndroidJavaObject currentActivity = unity.GetStatic<AndroidJavaObject>("currentActivity");
+        currentActivity.Call("startActivity", intentObject);
+        */
+    }
+
+    public static void ShareText(string text)
+    {
+        AndroidJavaClass intentClass = new AndroidJavaClass("android.content.Intent");
+        AndroidJavaObject intentObject = new AndroidJavaObject("android.content.Intent");
+
+        // Construct the intent.
+        AndroidJavaObject intent = new AndroidJavaObject("android.content.Intent");
+        intent.Call<AndroidJavaObject>("setAction", intentClass.GetStatic<string>("ACTION_SEND"));
+        intent.Call<AndroidJavaObject>("putExtra", intentClass.GetStatic<string>("EXTRA_TEXT"), text); //Here is the text to share
+        intent.Call<AndroidJavaObject>("setType", "text/plain");
+
+        // Display the chooser.
+        AndroidJavaClass unityPlayerClass = new AndroidJavaClass("com.unity3d.player.UnityPlayer");
+        AndroidJavaObject currentActivity = unityPlayerClass.GetStatic<AndroidJavaObject>("currentActivity");
+        AndroidJavaObject chooser = intentClass.CallStatic<AndroidJavaObject>("createChooser", intent, "Share");
+        currentActivity.Call("startActivity", chooser);
+    }
+
+}
